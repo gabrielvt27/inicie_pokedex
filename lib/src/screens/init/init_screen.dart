b08eb82da739c9eb27b33e1803fcdb8488edb28f
Gabriel Valentin <tiburcio_gabriel@outlook.com>
2022-05-25 21:01:24 -0300

    .

--- lib/src/screens/init/init_screen.dart
+++ lib/src/screens/init/init_screen.dart
@@ -14,78 +14,76 @@ class InitScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: AppStyles.kDefaultPadding,
-                  vertical: AppStyles.kDefaultPadding * 2,
-                ),
-                child: const PokeBanner(),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(
+                horizontal: AppStyles.kDefaultPadding,
+                vertical: AppStyles.kDefaultPadding,
               ),
-              const PokeBannerTitle(
-                textAlign: TextAlign.center,
+              child: const PokeBanner(),
+            ),
+            const PokeBannerTitle(
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: AppStyles.kDefaultPadding * 4,
+                vertical: AppStyles.kDefaultPadding,
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: AppStyles.kDefaultPadding * 4,
-                  vertical: AppStyles.kDefaultPadding * 2,
+              child: Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppStyles.kSecondaryTextColor.withOpacity(0.6),
+                      blurRadius: 15,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
                 ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: AppStyles.kSecondaryTextColor.withOpacity(0.6),
-                        blurRadius: 15,
-                        offset: const Offset(0, 4),
-                      ),
-                    ],
+                child: ElevatedButton(
+                  onPressed: () => Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomeScreen(),
+                    ),
                   ),
-                  child: ElevatedButton(
-                    onPressed: () => Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const HomeScreen(),
-                      ),
+                  style: ElevatedButton.styleFrom(
+                    primary: AppStyles.kSecondaryTextColor,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
                     ),
-                    style: ElevatedButton.styleFrom(
-                      primary: AppStyles.kSecondaryTextColor,
-                      elevation: 0,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 8,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Spacer(),
-                          Text(
-                            'Começar',
-                            style: GoogleFonts.nunito(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 18,
-                            ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Spacer(),
+                        Text(
+                          'Começar',
+                          style: GoogleFonts.nunito(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18,
                           ),
-                          const Spacer(),
-                          SvgPicture.asset(
-                            'assets/icons/arrow_rigth.svg',
-                          ),
-                        ],
-                      ),
+                        ),
+                        const Spacer(),
+                        SvgPicture.asset(
+                          'assets/icons/arrow_rigth.svg',
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
